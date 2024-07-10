8fc8255b413192ac804d7924aafb647781c919be
Shayan Ghosh <shayanghosh0308@gmail.com>
2024-07-10 18:45:15 +0530

    Changes in registration

--- controller/users.js
+++ controller/users.js
@@ -9,12 +9,13 @@ exports.register = async (req, res, next) => {
     if (isEmailExisted) throw createError(409, 'Email address already in Use');
 
     const userDetail = await users.create(payload);
-    const { user, error } = await supabase.auth.signUp({
+    const { data, error } = await supabase.auth.signUp({
       email: payload.email,
       password: payload.password,
     });
     if (error) throw createError(error.message);
-    return res.status(200).json({ message: "User Registered Successfully !!", User: userDetail.email });
+    console.log(data);
+    return res.status(200).json({ message: "User Registered Successfully !!" });
   } catch (error) {
     console.log("reg ====> ",error);
     next(error);
